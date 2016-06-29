5c13ab7d6360c1c3676b4ec04517497f7a9caaca
pandix73 <conan11conan112004@gmail.com>
2016-06-29 19:40:29 +0800

    6/29 update

---
+++ checkio/Home/checkio_Golden Pyramid.py
@@ -0,0 +1,36 @@
+def count_gold(pyramid):
+    list_pyramid = []
+    for layer in pyramid:
+        list_pyramid.append(list(layer))
+    for i in reversed(range(len(list_pyramid)-1)):
+        for j in range(len(list_pyramid[i])):
+            list_pyramid[i][j] += max(list_pyramid[i+1][j], list_pyramid[i+1][j+1]);
+    return list_pyramid[0][0]
+​
+​
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert count_gold((
+        (1,),
+        (2, 3),
+        (3, 3, 1),
+        (3, 1, 5, 4),
+        (3, 1, 3, 1, 3),
+        (2, 2, 2, 2, 2, 2),
+        (5, 6, 4, 5, 6, 4, 3)
+    )) == 23, "First example"
+    assert count_gold((
+        (1,),
+        (2, 1),
+        (1, 2, 1),
+        (1, 2, 1, 1),
+        (1, 2, 1, 1, 1),
+        (1, 2, 1, 1, 1, 1),
+        (1, 2, 1, 1, 1, 1, 9)
+    )) == 15, "Second example"
+    assert count_gold((
+        (9,),
+        (2, 2),
+        (3, 3, 3),
+        (4, 4, 4, 4)
+    )) == 18, "Third example"
